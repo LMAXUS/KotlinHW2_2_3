20bb9084f8ba353f28a684ef541cf99383e62cf1
MAXUS <l_maxus@mail.ru>
2023-09-30 22:10:28 +0700

    HW3_1_1 ver.4

--- src/test/kotlin/WallServiceTest.kt
+++ src/test/kotlin/WallServiceTest.kt
@@ -8,21 +8,8 @@ class WallServiceTest {
         WallService.clear()
     }
     @Test
-    fun add() {
-        val post = WallService.add(Post(0, 376, 56, 1692333801, "Начало"))
-        assertEquals(1, post.id)
-    }
-
-    @Test
-    fun updateIdExists() {
-        val post = WallService.add(Post(0, 376, 56, 1692333801, "Начало"))
-        val result = WallService.update(Post(1, 376, 56, 1692333801, "Самое начало"))
-        assertTrue(result)
-    }
-    @Test
-    fun updateIdDosntExists() {
-        val post = WallService.add(Post(0, 376, 56, 1692333801, "Начало"))
-        val result = WallService.update(Post(0, 376, 56, 1692333801, "Начало"))
-        assertFalse(result)
+    fun addPost() {
+        val post = WallService.addPost(10, "Тестовый пост", )
+        assertEquals(0, post.id)
     }
 }
